5beb92d58114778439f8bf5ae7097bde4a6e15d0
Cristián <cristian230677@gmail.com>
2023-01-31 12:09:33 -0300

    secondend

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require ('./app');
 require ('dotenv').config();
 const swaggerDocs = require ('../swagger')
-
+//port
 const PORT = process.env.PORT || 8000;
 
 app.listen (PORT, ()=> {
